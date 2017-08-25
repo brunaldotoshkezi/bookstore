0250ea6894bc3e13613e7cf06f285727b5536d30
brunaldotoshkezi <brunaldo.toshkezi@shish.gov.al>
2017-08-25 12:51:07 +0200

    example

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by brunaldo.toshkezi on 8/25/2017.
+ */
+var HeaderCtrl=function($scope){
+    $scope.appDetails={
+        title:"BooKart",
+        tagline:"We have 1 million books for you"
+    }
+
+}
